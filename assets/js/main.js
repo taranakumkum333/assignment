999ccd4450fe444e1bd703a17cbdac3fea564c3a
Tarana Akter <taranarahman2016@gmail.com>
2020-07-03 16:51:56 +0600

    added

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+
+$('.testi').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    navText: ['<i class="fa fa-chevron-left font_35" aria-hidden="true"></i>','<i class="fa fa-chevron-right font_35" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
